abd4c28c56aa43452cbe5ce302ab3799d3e4ee5c
iRobot98 <matendejudah@gmail.com>
2023-07-04 15:02:24 +0000

    working on app

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,19 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 function Nav() {
   const isUserLoggedIn = true;
+
+  const [providers, set_Providers] = useState(null);
+  const [dropDown, setDropDown] = useState(false);
+  const toggleDropDown = () => setDropDown(!dropDown);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      set_Providers(response);
+    };
+    setProviders();
+  }, []);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -27,9 +40,92 @@ function Nav() {
             <Link className="black_btn" href="/create-prompt">
               Create Post
             </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                alt="profile"
+                className="rounded-full"
+              />
+            </Link>
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={providers.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              className="rounded-full"
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              alt="profile"
+              onClick={toggleDropDown}
+            />
+            {toggleDropDown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setDropDown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setDropDown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  className="black_btn bg-black mt-5 w-full "
+                  onClick={() => {
+                    setDropDown(false);
+                    signOut();
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={providers.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
         )}
       </div>
     </nav>
--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,7 +1,9 @@
+"use client";
+import { SessionProvider } from "next-auth/react";
 import React from "react";
 
-function Provider() {
-  return <div>Provider</div>;
+function Provider({ children, session }) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
 
 export default Provider;
