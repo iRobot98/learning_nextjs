5747196c0871121afdfce77e6c668e316a12fac7
iRobot98 <matendejudah@gmail.com>
2023-07-05 13:43:43 +0000

    working on app

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,5 +1,7 @@
 "use client";
-import React, { useEffect, useState } from "react";
+
+import { useState, useEffect } from "react";
+
 import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
@@ -16,20 +18,55 @@ const PromptCardList = ({ data, handleTagClick }) => {
   );
 };
 
-function Feed({}) {
+const Feed = () => {
+  const [allPosts, setAllPosts] = useState([]);
+
+  // Search states
   const [searchText, setSearchText] = useState("");
-  const handleSearchChange = (e) => {
-    e.preventDefault();
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setAllPosts(data);
   };
 
-  const [post, setPost] = useState([]);
   useEffect(() => {
-    (async () => {
-      await fetch("/api/prompt")
-        .then((res) => res.json())
-        .then((d) => setPost(d));
-    })();
+    fetchPosts();
   }, []);
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -54,6 +91,6 @@ function Feed({}) {
       )}
     </section>
   );
-}
+};
 
 export default Feed;
--- models/user.models.js
+++ models/user.models.js
@@ -16,6 +16,7 @@ const UserSchema = new Schema({
   },
   image: {
     type: String,
+    default: "/assets/images/profile.png",
   },
 });
 
